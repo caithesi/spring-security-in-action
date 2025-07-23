eebea64e682e5f210762f3f66bc4f6782a081587
Cai The Si <caithesi@gmail.com>
2025-07-24 05:17:33 +0700

    implement user detail

---
+++ mycode/user-detail-implement/src/main/java/com/laurentiuspilca/ssia/config/UserManagementConfig.java
@@ -0,0 +1,31 @@
+package com.laurentiuspilca.ssia.config;
+
+import com.laurentiuspilca.ssia.model.User;
+import com.laurentiuspilca.ssia.services.InMemoryUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+/**
+ * this class was designed with two responsibilities for user management: UserDetailsService and PasswordEncoder
+ */
+@Configuration
+public class UserManagementConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails u = new User("john", "12345", "read");
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+//        return new BCryptPasswordEncoder();
+    }
+}
